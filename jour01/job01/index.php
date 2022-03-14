ac53dd40e7be2a8e10b5d04275e9be2ebe38d3ae
Thomas <thomas.serdjebi@laplateforme.io>
2022-03-14 12:46:15 +0100

    Jour01/Job01 ok

---
+++ jour01/job01/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+    <meta charset="utf-8">
+    <title>Titre de la page</title>
+    <link rel="stylesheet" href="style.css">
+    <script type="text/javascript">
+        alert('Hello Javascript!');
+    </script>
+    
+    </head>
+
+    <body>
+
+    </body>
+</html>
