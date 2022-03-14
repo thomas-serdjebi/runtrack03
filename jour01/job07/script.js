dd469640c14c1747d3c5d3e2ca19e4fc0196ea57
Thomas <thomas.serdjebi@laplateforme.io>
2022-03-14 14:23:16 +0100

    jour01/job05 ok

---
+++ jour01/job07/script.js
@@ -0,0 +1,49 @@
+function jourtravail(date){
+
+    var a = date;
+
+    f=false;
+    w=false;
+
+    tabDate =[
+    new Date(2022,0,1),
+    new Date(2022,4,13),
+    new Date(2022,5,1),
+    new Date(2022,5,8),
+    new Date(2022,5,21),
+    new Date(2022,6,1),
+    new Date(2022,7,14),
+    new Date(2022,8,15),
+    new Date(2022,11,1),
+    new Date(2022,11,11),
+    new Date(2022,11,25)
+    ] ;
+
+    for(var i=0 ; i<tabDate.length ; i++)
+    {
+        if(tabDate[i].getDate()==a.getDate()&&tabDate[i].getMonth()==a.getMonth()&&tabDate[i].getFullYear()==a.getFullYear())
+        {
+            console.log("Le ",a.getDate(),"/",a.getMonth()+1,"/",a.getFullYear(), "est un jour férié");
+            f=true;
+            break;
+        }
+    }
+    
+    if(f==false)
+    {
+        if(a.getDay()==6||a.getDay()==0)
+        {
+            console.log("Non, ",a.getDate(),"/",a.getMonth()+1,"/",a.getFullYear()," est un week-end");
+            w=true;
+        }
+        if(w==false)
+        {
+            console.log("Oui, Le ",a.getDate(),"/",a.getMonth()+1,"/",a.getFullYear()," est un jour travaillé");
+        }
+    }
+
+}
+var j = new Date("may 01 2022");
+jourtravail(j);
+
+
