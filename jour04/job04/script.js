d6e8a74fdb8ccf8a1aa62846531b56f2bea896ba
Thomas <thomas.serdjebi@laplateforme.io>
2022-03-21 14:54:30 +0100

    jour04 Ok

--- jour04/job04/script.js
+++ jour04/job04/script.js
@@ -1,29 +1,41 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+    let button = document.getElementById("update");
+
+    button.addEventListener("click", function() {
+        location.reload()
+    })
 
     fetch('users.php')
     .then(response => response.json())
     .then(response => {
 
-       
         let btnUpdate = document.getElementById('update')
         let table = document.querySelector('table')
         let table_body = document.querySelector('tbody')
-        let ligne = document.createElement('tr')
-        let cellule = document.createElement('td')
+        
+        response.forEach((element)=> {
+            let ligne = document.createElement('tr')
+            let id_column = document.createElement('td')
+            table_body.append(ligne)
+            ligne.append(id_column)
+            id_column.append(element.id)
 
+            let prenom_column = document.createElement('td')
+            ligne.append(prenom_column)
+            prenom_column.append(element.prenom)
 
-        response.forEach(element => 
-            table_body.append(ligne),
-            ligne.append(cellule),
-            cellule.append(response.id)
-        )
+            let nom_column = document.createElement('td')
+            ligne.append(nom_column),
+            nom_column.append(element.nom)
 
+            let email_column = document.createElement('td')
+            ligne.append(email_column),
+            email_column.append(element.email)
 
+        })
 
     })
 
-
     
-
 
 })
